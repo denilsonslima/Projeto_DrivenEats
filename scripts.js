7a5f4ab24c2a13a50ba5e4f06632b1f0225df335
denilsonslima <denilsondslima@gmail.com>
2022-10-16 20:42:15 -0300

    WhatsAppa

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
 let comida, preco, valor1, valor2, valor3, final1, final2, final3;
-let i = 0;
 function seção_1 (seletor){
     const bordaSelect = document.querySelector(".borda1");
     if (bordaSelect !== null){
         bordaSelect.classList.remove("borda1")
     }
-    seletor.classList.add("borda1"); 
+    seletor.classList.add("borda1")
+
+    const apagar = document.querySelector(".seuPrato .aparecer");
+    const aparecer = document.querySelector(".borda1 .escondido");
+    aparecer.classList.add("aparecer")
+    if (apagar !== null){
+        apagar.classList.remove("aparecer")
+        seletor.classList.remove("borda1")
+    }
+    
+
+    // console.log(bordaSelect)
+
+
     const prato = document.querySelector(".borda1>h1")
     comida = prato.innerHTML;
     const prato2 = document.querySelector(".borda1>div>h2")
@@ -13,14 +25,6 @@ function seção_1 (seletor){
     const res = preco.replace("R$ ", "")
     valor1 = res.replace(",", ".")
     final1 = Number(valor1)
-
-    const apagar = document.querySelector(".seuPrato .aparecer");
-    const aparecer = document.querySelector(".borda1 .escondido");
-    aparecer.classList.add("aparecer")
-    if (apagar !== null){
-        apagar.classList.remove("aparecer")
-        bordaSelect.classList.remove("borda1")
-    }
 
     ativarBotao ()
 }
@@ -47,7 +51,6 @@ function seção_2 (seletor){
     if (apagar !== null){
         apagar.classList.remove("aparecer")
         bordaSelect.classList.remove("borda2")
-        comida = undefined;
     }
 
     ativarBotao ()
@@ -59,6 +62,7 @@ function seção_3 (seletor){
     if (bordaSelect !== null){
         bordaSelect.classList.remove("borda3")
     }
+    console.log(bordaSelect)
     seletor.classList.add("borda3")
     const prato = document.querySelector(".borda3>h1")
     sobremesa = prato.innerHTML;
@@ -78,20 +82,15 @@ function seção_3 (seletor){
     ativarBotao ()
 }
 
-let botao;
+let botao, texto;
 function ativarBotao (){
     if (comida !== undefined && bebida !== undefined && sobremesa !== undefined){
         botao = document.querySelector(".roda-pe>button");
         botao.classList.add("ativadorBotao")
         
-        const texto = document.querySelector(".ativar")
+        texto = document.querySelector(".ativar")
+        texto.innerHTML = "Fechar pedido"
 
-        const p = document.querySelector(".ativar p")
-        texto.removeChild(p)
-        
-        const final = document.querySelector(".finalizado")
-        final.classList.add("final")
-        enviar()
     } else {
         botao.classList.remove("ativadorBotao")
     }
@@ -99,4 +98,14 @@ function ativarBotao (){
 
 function enviar (){
     const soma = final1 + final2 + final3;
+
+    let mensagem = encodeURIComponent(`
+    Olá, gostaria de fazer o *pedido*:\n*- Prato*: ${comida}\n*- Bebida*: ${bebida}\n*- Sobremesa*: ${sobremesa}\n*Total*: R$ ${soma}\n
+    `)
+
+    if (texto !== undefined){
+        window.open("http://wa.me/5598984358019?text=" + mensagem)
+    }
+
+    
 }
